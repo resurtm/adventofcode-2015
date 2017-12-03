f3e47783f95e62ee796ebde40aea0a4a6f8b73e5
resurtm <resurtm@gmail.com>
2017-12-04 03:24:22 +0600

    problem 4, part 1

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/resurtm/adventofcode-2015/problem1"
 	"github.com/resurtm/adventofcode-2015/problem2"
 	"github.com/resurtm/adventofcode-2015/problem3"
+	"github.com/resurtm/adventofcode-2015/problem4"
 )
 
 func main() {
@@ -21,6 +22,9 @@ func main() {
 	case "--problem3":
 		problem3.RunPartOne()
 		problem3.RunPartTwo()
+	case "--problem4":
+		problem4.RunPartOne()
+		problem4.RunPartTwo()
 	default:
 		panic("nothing to do")
 	}
---
+++ problem4/main.go
@@ -0,0 +1,65 @@
+package problem4
+
+import (
+	"fmt"
+	"crypto/md5"
+)
+
+type testCase struct {
+	input  string
+	result int
+}
+
+var testCasesPartOne = []testCase{
+	{input: "abcdef", result: 609043},
+	{input: "pqrstuv", result: 1048970},
+	{input: "iwrupvqb", result: -1},
+}
+
+var testCasesPartTwo = []testCase{
+}
+
+func RunPartOne() {
+	fmt.Printf(">>> PART ONE <<<\n\n")
+	for k, v := range testCasesPartOne {
+		fmt.Printf("test case : %d\n", k)
+		if len(v.input) > 250 {
+			fmt.Printf("input     : [TOO LONG VALUE]\n")
+		} else {
+			fmt.Printf("input     : %s\n", v.input)
+		}
+		fmt.Printf("expected  : %d\n", v.result)
+		fmt.Printf("result    : %d\n\n", v.solvePartOne())
+	}
+}
+
+func RunPartTwo() {
+	fmt.Printf(">>> PART TWO <<<\n\n")
+	for k, v := range testCasesPartTwo {
+		fmt.Printf("test case : %d\n", k)
+		if len(v.input) > 250 {
+			fmt.Printf("input     : [TOO LONG VALUE]\n")
+		} else {
+			fmt.Printf("input     : %s\n", v.input)
+		}
+		fmt.Printf("expected  : %d\n", v.result)
+		fmt.Printf("result    : %d\n\n", v.solvePartTwo())
+	}
+}
+
+func (tc *testCase) solvePartOne() int {
+	i := 0
+	for {
+		x := fmt.Sprintf("%s%d", tc.input, i)
+		hash := fmt.Sprintf("%x", md5.Sum([]byte(x)))
+		if hash[:5] == "00000" {
+			break
+		}
+		i++
+	}
+	return i
+}
+
+func (tc *testCase) solvePartTwo() int {
+	return 0
+}
